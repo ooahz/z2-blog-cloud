4344aba88d59244333cb6a672333267e7ad5d417
十玖八柒(ahzoo) <1359256243@qq.com>
2024-06-24 20:45:36 +0800

    perf: 处理文章代码块语言为空的情况
    (cherry picked from commit ac3d9e3d80c7cd6de2825072740ab35dd2719c9f)

---
+++ blog-admin/src/main/java/cn/ahzoo/admin/utils/ArticleUtil.java
@@ -0,0 +1,35 @@
+package cn.ahzoo.admin.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ArticleUtil {
+
+    private static final String ID_PREFIX = "header-";
+
+    public static String formatHtml(String html) {
+        Document document = Jsoup.parse(html, "UTF-8");
+        Elements bodyElements = document.select("body");
+        bodyElements.forEach(bodyElement -> {
+            Elements bodyChildrenElements = bodyElement.children();
+            AtomicInteger index = new AtomicInteger(0);
+            bodyChildrenElements.forEach(element -> {
+                String tagName = element.tagName();
+                formatToc(tagName, element, index);
+            });
+        });
+        return bodyElements.first().html()
+                .replace("<pre><code>", "<pre><code class='language-text'>");
+    }
+
+    public static void formatToc(String tagName, Element element, AtomicInteger index) {
+        if (tagName.startsWith("h") && !"hr".equals(tagName)) {
+            element.attr("id", ID_PREFIX + index.getAndIncrement());
+        }
+    }
+
+}
