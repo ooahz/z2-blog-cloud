a2273abb8d4b14b08d215425d31e07ce29535cd9
十玖八柒(ahzoo) <1359256243@qq.com>
2024-08-12 17:04:01 +0800

    fix: 修复admin模块swagger-ui问题
    (cherry picked from commit 31b60d25b5046ac66e268be81eedcb26af00438a)

---
+++ blog-admin/src/main/java/cn/ahzoo/admin/config/JsonModuleConfig.java
@@ -0,0 +1,23 @@
+package cn.ahzoo.admin.config;
+
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 十玖八柒（Ahzoo）
+ * @description JSON数据类型转换，Long类型转为String类型
+ * @github https://github.com/ooahz
+ * @date 2024/7
+ */
+@Configuration
+public class JsonModuleConfig extends SimpleModule {
+
+    public JsonModuleConfig() {
+        // 序列化
+        this.addSerializer(Long.class, ToStringSerializer.instance);
+        this.addSerializer(Long.TYPE, ToStringSerializer.instance);
+    }
+
+}
+
